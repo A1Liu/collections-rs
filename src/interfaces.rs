bc0ba82a1337c58a7cf3244ac56986192824fa83
Albert Liu <albertymliu@gmail.com>
2019-05-08 16:56:08 -0400

    added more stuffs

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -2,6 +2,38 @@ use crate::generic_containers::*;
 use core::borrow::Borrow;
 use core::ops::{Index, IndexMut};
 
+/// Trait for a container indexed by a value that implements copy.
+pub trait CopyMap<K, V, E = (K, V)>: Container<E>
+where
+    K: Copy + Eq,
+{
+    /// Get a value from this Map. Takes a key by reference
+    /// and returns a reference to the corresponding data,
+    /// or `None` if none exists.
+    fn get(&self, key: K) -> Option<&V>;
+
+    /// Returns true if this container contains the key.
+    fn contains<Q: ?Sized>(&self, key: K) -> bool {
+        match self.get(key) {
+            Some(_) => true,
+            None => false,
+        }
+    }
+
+    /// Returns a mutable reference to an object stored in
+    /// this container based on the key given, or `None` if
+    /// the key does not exist.
+    fn get_mut(&mut self, key: K) -> Option<&mut V>;
+
+    /// Adds a new item into this container with the associated key,
+    /// and returns the previous value associated with that key, if it existed.
+    fn insert(&mut self, k: K, v: V) -> Option<V>;
+
+    /// Removes an item from this container using the associated key,
+    /// and returns it (if it existed).
+    fn remove(&mut self, k: K) -> Option<V>;
+}
+
 /// Trait for a container indexed by a value. The value that
 /// indexes the container must implement the Eq trait to.
 /// Supports insertion,
@@ -43,7 +75,19 @@ where
 
     /// Removes an item from this container using the associated key,
     /// and returns it (if it existed).
-    fn remove<Q: ?Sized>(&mut self, k: &Q) -> Option<V>;
+    fn remove<Q: ?Sized>(&mut self, k: &Q) -> Option<V>
+    where
+        K: Borrow<Q>,
+        Q: Eq;
+}
+
+/// Key-value map that also uses the `[`bracket`]` operators to access and modify
+/// the internal data.
+pub trait CopyDictionary<K, V>:
+    CopyMap<K, V, K> + DynamicContainer<K> + Index<K, Output = V> + IndexMut<K, Output = V>
+where
+    K: Copy + Eq,
+{
 }
 
 /// Key-value map that also uses the `[`bracket`]` operators to access and modify
@@ -56,10 +100,13 @@ where
 }
 
 /// Statically-sized array stored in the heap.
-pub trait Array<V>: Map<usize, V> + Index<usize, Output = V> + IndexMut<usize, Output = V> {}
+pub trait Array<V>:
+    CopyMap<usize, V> + Index<usize, Output = V> + IndexMut<usize, Output = V>
+{
+}
 
 /// Dynamically changing array of values.
 pub trait DynamicArray<V>:
-    Map<usize, V, V> + DynamicContainer<V> + Index<usize, Output = V> + IndexMut<usize, Output = V>
+    CopyMap<usize, V, V> + DynamicContainer<V> + Index<usize, Output = V> + IndexMut<usize, Output = V>
 {
 }
