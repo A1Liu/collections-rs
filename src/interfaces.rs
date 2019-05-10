b0fbb0aa630c4d517dd81e5baf0bb80eb7ee56d1
Albert Liu <albertymliu@gmail.com>
2019-05-10 17:43:51 -0400

    made interfaces more flexible

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -1,6 +1,6 @@
 use crate::generic_containers::*;
 use core::borrow::Borrow;
-use core::ops::{Index, IndexMut};
+use core::ops::{Deref, DerefMut, Index, IndexMut};
 
 /// Trait for a container indexed by a value that implements `Copy` and `Eq`.
 pub trait CopyMap<K, V, E = (K, V)>: Container<E>
@@ -10,12 +10,16 @@ where
     /// Get a value from this Map. Takes a key by reference
     /// and returns a reference to the corresponding data,
     /// or `None` if none exists.
-    fn get(&self, key: K) -> Option<&V>;
+    fn get<R>(&self, key: K) -> Option<R>
+    where
+        R: Deref<Target = V>;
 
     /// Returns a mutable reference to an object stored in
     /// this container based on the key given, or `None` if
     /// the key does not exist.
-    fn get_mut(&mut self, key: K) -> Option<&mut V>;
+    fn get_mut<R>(&mut self, key: K) -> Option<R>
+    where
+        R: DerefMut<Target = V>;
 
     /// Adds a new item into this container with the associated key,
     /// and returns the previous value associated with that key, if it existed.
@@ -30,18 +34,20 @@ where
     /// Get a value from this Map. Takes a key by reference
     /// and returns a reference to the corresponding data,
     /// or `None` if none exists.
-    fn get<Q: ?Sized>(&self, key: &Q) -> Option<&V>
+    fn get<Q, R>(&self, key: &Q) -> Option<R>
     where
         K: Borrow<Q>,
-        Q: Eq;
+        Q: Eq + ?Sized,
+        R: Deref<Target = V>;
 
     /// Returns a mutable reference to an object stored in
     /// this container based on the key given, or `None` if
     /// the key does not exist.
-    fn get_mut<Q: ?Sized>(&mut self, k: &Q) -> Option<&mut V>
+    fn get_mut<Q, R>(&mut self, k: &Q) -> Option<R>
     where
         K: Borrow<Q>,
-        Q: Eq;
+        Q: Eq + ?Sized,
+        R: DerefMut<Target = V>;
 
     /// Adds a new item into this container with the associated key,
     /// and returns the previous value associated with that key, if it existed.
@@ -56,8 +62,11 @@ where
     K: Copy + Eq,
 {
     /// Returns true if this container contains the key.
-    fn contains(&self, key: K) -> bool {
-        match self.get(key) {
+    fn contains<R>(&self, key: K) -> bool
+    where
+        R: Deref<Target = V>,
+    {
+        match self.get::<R>(key) {
             Some(_) => true,
             None => false,
         }
@@ -76,12 +85,13 @@ where
     K: Eq,
 {
     /// Returns true if this container contains the key.
-    fn contains<Q: ?Sized>(&self, key: &Q) -> bool
+    fn contains<Q, R>(&self, key: &Q) -> bool
     where
         K: Borrow<Q>,
-        Q: Eq,
+        Q: Eq + ?Sized,
+        R: Deref<Target = V>,
     {
-        match self.get(key) {
+        match self.get::<Q, R>(key) {
             Some(_) => true,
             None => false,
         }
