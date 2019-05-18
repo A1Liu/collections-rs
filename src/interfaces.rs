c15e185d7631fbbc555baa93e5447fb8c6d4edf7
Albert Liu <albertymliu@gmail.com>
2019-05-17 23:15:35 -0400

    more of the simple

--- src/interfaces.rs
+++ src/interfaces.rs
@@ -1,23 +1,21 @@
 use crate::generic_containers::*;
 use core::borrow::Borrow;
-use core::ops::{Deref, DerefMut, Index, IndexMut};
+use core::ops::{Index, IndexMut};
 
 /// Trait for a container indexed by a value that implements `Copy` and `Eq`.
-pub trait CopyMap<K, V, R, Rm, E = (K, V)>: Container<E>
+pub trait CopyMap<K, V, E = (K, V)>: Container<E>
 where
     K: Copy + Eq,
-    R: Deref<Target = V>,
-    Rm: DerefMut<Target = V>,
 {
     /// Get a value from this Map. Takes a key by reference
     /// and returns a reference to the corresponding data,
     /// or `None` if none exists.
-    fn get(&self, key: K) -> Option<R>;
+    fn get(&self, key: K) -> Option<&E>;
 
     /// Returns a mutable reference to an object stored in
     /// this container based on the key given, or `None` if
     /// the key does not exist.
-    fn get_mut(&mut self, key: K) -> Option<Rm>;
+    fn get_mut(&mut self, key: K) -> Option<&mut E>;
 
     /// Adds a new item into this container with the associated key,
     /// and returns the previous value associated with that key, if it existed.
@@ -25,16 +23,14 @@ where
 }
 
 /// Trait for a container indexed by a value that implements `Eq`.
-pub trait Map<K, V, R, Rm, E = (K, V)>: Container<E>
+pub trait Map<K, V, E = (K, V)>: Container<E>
 where
     K: Eq,
-    R: Deref<Target = V>,
-    Rm: DerefMut<Target = V>,
 {
     /// Get a value from this Map. Takes a key by reference
     /// and returns a reference to the corresponding data,
     /// or `None` if none exists.
-    fn get<Q: ?Sized>(&self, key: &Q) -> Option<R>
+    fn get<Q: ?Sized>(&self, key: &Q) -> Option<&E>
     where
         K: Borrow<Q>,
         Q: Eq;
@@ -42,7 +38,7 @@ where
     /// Returns a mutable reference to an object stored in
     /// this container based on the key given, or `None` if
     /// the key does not exist.
-    fn get_mut<Q: ?Sized>(&mut self, k: &Q) -> Option<Rm>
+    fn get_mut<Q: ?Sized>(&mut self, k: &Q) -> Option<&mut E>
     where
         K: Borrow<Q>,
         Q: Eq;
@@ -54,12 +50,10 @@ where
 
 /// Key-value map that also uses the `[`bracket`]` operators to access and modify
 /// the internal data.
-pub trait CopyDictionary<K, V, R, Rm>:
-    CopyMap<K, V, R, Rm, V> + DynamicContainer<(K, V)> + Index<K, Output = V> + IndexMut<K, Output = V>
+pub trait CopyDictionary<K, V>:
+    CopyMap<K, V, V> + DynamicContainer<(K, V)> + Index<K, Output = V> + IndexMut<K, Output = V>
 where
     K: Copy + Eq,
-    R: Deref<Target = V>,
-    Rm: DerefMut<Target = V>,
 {
     /// Returns true if this container contains the key.
     fn contains(&self, key: K) -> bool {
@@ -77,11 +71,9 @@ where
 /// Key-value map that also uses the `[`bracket`]` operators to access and modify
 /// the internal data.
 pub trait Dictionary<K, V, R, Rm>:
-    Map<K, V, R, Rm, (K, V)> + DynamicContainer<K> + Index<K, Output = V> + IndexMut<K, Output = V>
+    Map<K, V, (K, V)> + DynamicContainer<K> + Index<K, Output = V> + IndexMut<K, Output = V>
 where
     K: Eq,
-    R: Deref<Target = V>,
-    Rm: DerefMut<Target = V>,
 {
     /// Returns true if this container contains the key.
     fn contains<Q: ?Sized>(&self, key: &Q) -> bool
@@ -104,22 +96,13 @@ where
 }
 
 /// Statically-sized array stored in the heap.
-pub trait Array<V, R, Rm>:
-    CopyMap<usize, V, R, Rm, (usize, V)> + Index<usize, Output = V> + IndexMut<usize, Output = V>
-where
-    R: Deref<Target = V>,
-    Rm: DerefMut<Target = V>,
+pub trait Array<V>:
+    CopyMap<usize, V, (usize, V)> + Index<usize, Output = V> + IndexMut<usize, Output = V>
 {
 }
 
 /// Dynamically changing array of values.
-pub trait DynamicArray<V, R, Rm>:
-    CopyMap<usize, V, R, Rm, V>
-    + DynamicContainer<V>
-    + Index<usize, Output = V>
-    + IndexMut<usize, Output = V>
-where
-    R: Deref<Target = V>,
-    Rm: DerefMut<Target = V>,
+pub trait DynamicArray<V>:
+    CopyMap<usize, V, V> + DynamicContainer<V> + Index<usize, Output = V> + IndexMut<usize, Output = V>
 {
 }
